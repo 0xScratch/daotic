0415241a32b7e1545ecc48d9e37b153f632ce734
Olivier <olivier.winkler@icloud.com>
2023-08-05 19:55:46 +0200

    feat(contracts): bump solidity version to 0.8.18 & add named imports

--- src/swissDAO.sol
+++ src/swissDAO.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: Proprietary
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155} from "@oz/token/ERC1155/ERC1155.sol";
+import {Strings} from "@oz/utils/Strings.sol";
 
-contract DAONation is DAOManagement, ERC1155Burnable {
+contract DAONation is ERC1155 {
     // uint256 public constant WRAPPED_ETH = 0;
     uint256 public constant EXPERIENCE_POINTS = 1;
     uint256 public constant ACTIVITY_POINTS = 2;
     uint256 public constant ASSIGNED_ROLES = 3;
 
-    constructor() public ERC1155("https://swissdao.space/api/item/{id}.json") {
+    constructor() ERC1155("https://swissdao.space/api/item/{id}.json") {
         // _mint(msg.sender, NOTHING, 10**18, "");
     }
 
