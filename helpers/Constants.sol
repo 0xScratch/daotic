a086a2332a54924fcc23333f8d05dacb1fdfb499
Olivier <olivier.winkler@icloud.com>
2023-08-16 18:43:59 +0200

    feat(helpers): add helpers constant contract

---
+++ helpers/Constants.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/// @title Constants
+/// @author swissdao.space (https://github.com/swissDAO)
+/// @notice Constants Contract for Tests
+/// @custom:security-contact xxx@gmail.com
+contract Constants {
+    /*//////////////////////////////////////////////////////////////
+                                CONSTANTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    address public DEFAULT_ADMIN_ROLER = address(69420);
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    address public CORE_DELEGATE_ROLER = address(100000);
+}
