7d41be632e2e0b645081aa8668cb9ce49cec8392
Olivier <olivier.winkler@icloud.com>
2023-08-25 15:20:46 +0200

    feat(membership): format code

--- src/SwissDAO.sol
+++ src/SwissDAO.sol
@@ -85,25 +85,27 @@ contract SwissDAO is ERC1155, AccessControl {
                                PUBLIC
     //////////////////////////////////////////////////////////////*/
 
-
     /// @notice Explain to a developer any extra details
     /// @dev Explain to a developer any extra details
     /// @param _tokenid Sender's Address
     function uri(uint256 _tokenid) public view override returns (string memory) {
-
         bytes memory _svg;
         string memory _name;
         string memory _description;
 
-        if(_tokenid == 1) {
-            _svg = abi.encodePacked('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 1000 1000" width="1000" height="1000" version="1.1"><rect x="0" y="0" width="1000" height="1000" fill="#001122"/><path d="M0 838L143 716L286 631L429 765L571 726L714 745L857 760L1000 652L1000 1001L857 1001L714 1001L571 1001L429 1001L286 1001L143 1001L0 1001Z" fill="#C62368" stroke-linecap="square" stroke-linejoin="bevel"/><text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="500" letter-spacing="-0.04em" x="200" y="600">XP</text></svg>');
+        if (_tokenid == 1) {
+            _svg = abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 1000 1000" width="1000" height="1000" version="1.1"><rect x="0" y="0" width="1000" height="1000" fill="#001122"/><path d="M0 838L143 716L286 631L429 765L571 726L714 745L857 760L1000 652L1000 1001L857 1001L714 1001L571 1001L429 1001L286 1001L143 1001L0 1001Z" fill="#C62368" stroke-linecap="square" stroke-linejoin="bevel"/><text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="500" letter-spacing="-0.04em" x="200" y="600">XP</text></svg>'
+            );
             _name = "XP";
             _description = "Experience Point";
-        }else if(_tokenid == 2) {
-            _svg = abi.encodePacked('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 1000 1000" width="1000" height="1000" version="1.1"><rect x="0" y="0" width="1000" height="1000" fill="#001122"/><path d="M0 838L143 716L286 631L429 765L571 726L714 745L857 760L1000 652L1000 1001L857 1001L714 1001L571 1001L429 1001L286 1001L143 1001L0 1001Z" fill="#C62368" stroke-linecap="square" stroke-linejoin="bevel"/><text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="500" letter-spacing="-0.04em" x="200" y="600">AP</text></svg>');
+        } else if (_tokenid == 2) {
+            _svg = abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="visual" viewBox="0 0 1000 1000" width="1000" height="1000" version="1.1"><rect x="0" y="0" width="1000" height="1000" fill="#001122"/><path d="M0 838L143 716L286 631L429 765L571 726L714 745L857 760L1000 652L1000 1001L857 1001L714 1001L571 1001L429 1001L286 1001L143 1001L0 1001Z" fill="#C62368" stroke-linecap="square" stroke-linejoin="bevel"/><text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="500" letter-spacing="-0.04em" x="200" y="600">AP</text></svg>'
+            );
             _name = "AP";
             _description = "Activity Point";
-        }else{
+        } else {
             address member = address(uint160(_tokenid)); // membership NFT tokens inherit their id from their owners address
             uint256 xp = balanceOf(member, EXPERIENCE_POINTS);
             uint256 ap = balanceOf(member, ACTIVITY_POINTS);
@@ -111,41 +113,58 @@ contract SwissDAO is ERC1155, AccessControl {
             // bool hasProjectManagerRole = hasRole(PROJECT_MANAGER_ROLE, member);
 
             _svg = abi.encodePacked(
-            '<svg ',
+                "<svg ",
                 'xmlns="http://www.w3.org/2000/svg" ',
                 'width="1000" ',
                 'height="1000" ',
                 'fill="#1E1E1E" ',
                 'stroke="#ffffff"',
-            '>',
+                ">",
                 '<rect width="1000" height="1000" fill="#1E1E1E" />',
                 '<rect x="75.5" y="140.5" width="349" height="219" rx="11.5" fill="black" stroke="white" />',
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="104" y="187">', "Holder", "</tspan></text>",
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="250" y="187">', Strings.toHexString(member), "</tspan></text>",
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="104" y="225">', "AP", "</tspan></text>",
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="250" y="225">', Strings.toString(ap), "</tspan></text>",
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="104" y="265">', "XP", "</tspan></text>",
-                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">', '<tspan x="250" y="265">', Strings.toString(xp), "</tspan></text>",
-            '</svg>'
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="104" y="187">',
+                "Holder",
+                "</tspan></text>",
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="250" y="187">',
+                Strings.toHexString(member),
+                "</tspan></text>",
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="104" y="225">',
+                "AP",
+                "</tspan></text>",
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="250" y="225">',
+                Strings.toString(ap),
+                "</tspan></text>",
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="104" y="265">',
+                "XP",
+                "</tspan></text>",
+                '<text fill="white" xml:space="preserve" style="white-space: pre" font-family="Arial" font-size="24" letter-spacing="-0.04em">',
+                '<tspan x="250" y="265">',
+                Strings.toString(xp),
+                "</tspan></text>",
+                "</svg>"
             );
-            
+
             _name = "Membership";
             _description = "swissDAO Membership";
         }
 
-
         bytes memory _metadata = abi.encodePacked(
             "{",
-                '"name": "',
-                _name,
-                '",',
-                '"description": "',
-                _description,
-                '", ',
-                '"image": ',
-                '"data:image/svg+xml;base64,',
-                Base64.encode(_svg),
-                '",',
+            '"name": "',
+            _name,
+            '",',
+            '"description": "',
+            _description,
+            '", ',
+            '"image": ',
+            '"data:image/svg+xml;base64,',
+            Base64.encode(_svg),
+            '",',
             "}"
         );
         return string(abi.encodePacked("data:application/json;base64,", Base64.encode(_metadata)));
@@ -154,7 +173,10 @@ contract SwissDAO is ERC1155, AccessControl {
     /// This function increases points for a specified member by a specified ammount. Only core delegates or community manegers can increase experience points.
     function increasePoints(address member, uint256 amount) public {
         // Check that the calling account has the CORE_DELEGATE_ROLE or COMMUNITY_MANAGER_ROLE
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender) && !hasRole(COMMUNITY_MANAGER_ROLE, msg.sender)) {
+        if (
+            !hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender)
+                && !hasRole(COMMUNITY_MANAGER_ROLE, msg.sender)
+        ) {
             revert SwissDAO_PermissionError();
         }
 
@@ -165,12 +187,15 @@ contract SwissDAO is ERC1155, AccessControl {
     /// This function decreases activity points for all members by one. Only core delegates or community manegers can increase experience points.
     function decreaseActivityPoints() public {
         // Check that the calling account has the CORE_DELEGATE_ROLE or COMMUNITY_MANAGER_ROLE
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender) && !hasRole(COMMUNITY_MANAGER_ROLE, msg.sender)) {
+        if (
+            !hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender)
+                && !hasRole(COMMUNITY_MANAGER_ROLE, msg.sender)
+        ) {
             revert SwissDAO_PermissionError();
         }
 
-        for(uint i=0; i<s_members.length;){
-            if(balanceOf(s_members[i], ACTIVITY_POINTS)>0){
+        for (uint256 i = 0; i < s_members.length;) {
+            if (balanceOf(s_members[i], ACTIVITY_POINTS) > 0) {
                 _burn(s_members[i], ACTIVITY_POINTS, 1);
             }
             unchecked {
@@ -178,19 +203,21 @@ contract SwissDAO is ERC1155, AccessControl {
             }
         }
     }
-    
 
     /// This function onboards a new member. Only core delegates or event manegers can onboard members.
     function onboard(address member) public {
         // Check that the calling account has the CORE_DELEGATE_ROLE or EVENT_MANAGER_ROLE
-        if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender) && !hasRole(EVENT_MANAGER_ROLE, msg.sender)) {
+        if (
+            !hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(CORE_DELEGATE_ROLE, msg.sender)
+                && !hasRole(EVENT_MANAGER_ROLE, msg.sender)
+        ) {
             revert SwissDAO_PermissionError();
         }
-        
+
         uint256 membershipId = uint256(uint160(member));
 
         // Check if member is not already onboarded
-        if(balanceOf(member, membershipId) == 1 ) {
+        if (balanceOf(member, membershipId) == 1) {
             revert SwissDAO_OnboardingError();
         }
 
